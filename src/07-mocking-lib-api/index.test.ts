b53b63f1d7d1b10efc4a0e49c0da25af1d725955
Alena <alenzija@gmail.com>
2024-02-12 18:03:18 +0100

    feat: add some mocking lib api tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,15 +1,6 @@
 // Uncomment the code below and write your tests
+import axios from 'axios';
 import { throttledGetDataFromApi } from './index';
-
-jest.mock('axios', () => ({
-  create: () => ({
-    get: jest.fn().mockResolvedValue({ data: { id: 1, title: 'Test Title' } }),
-  }),
-}));
-
-// const createMock = (jest.spyOn(axios, 'create') as jest.Mock).mockReturnValue({
-//   get: jest.fn().mockResolvedValue({ data: { id: 1, title: 'Test Title' } }),
-// });
 
 describe('throttledGetDataFromApi', () => {
   beforeAll(() => {
@@ -25,22 +16,34 @@ describe('throttledGetDataFromApi', () => {
   });
 
   test('should create instance with provided base url', async () => {
+    const createMock = jest.spyOn(axios, 'create');
+    (jest.spyOn(axios.Axios.prototype, 'get') as jest.Mock).mockResolvedValue({
+      data: { id: 1, title: 'Test Title' },
+    });
     const pathFile = 'posts/1';
     await throttledGetDataFromApi(pathFile);
-    // expect(createMock).toHaveBeenCalledWith({
-    //   baseURL: 'https://jsonplaceholder.typicode.com',
-    // });
+    jest.runAllTimers();
+    expect(createMock).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
+    jest.spyOn(axios, 'create');
+    const getMock = (
+      jest.spyOn(axios.Axios.prototype, 'get') as jest.Mock
+    ).mockResolvedValue({ data: { id: 1, title: 'Test Title' } });
     const relativePath = '/posts/1';
     await throttledGetDataFromApi(relativePath);
-    // expect(createMock().get).toHaveBeenCalledWith(
-    //   'https://jsonplaceholder.typicode.com/posts/1',
-    // );
+    jest.runAllTimers();
+    expect(getMock).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
+    jest.spyOn(axios, 'create');
+    (jest.spyOn(axios.Axios.prototype, 'get') as jest.Mock).mockResolvedValue({
+      data: { id: 1, title: 'Test Title' },
+    });
     const relativePath = '/posts/1';
     const data = await throttledGetDataFromApi(relativePath);
     expect(data).toEqual({ id: 1, title: 'Test Title' });
